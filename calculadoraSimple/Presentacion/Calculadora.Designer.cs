ed12c3fd8de3740ffd3a6d646fe99c2c5bf8b0af
Isaactavaresponce <isaactavares50@gmail.com>
2024-03-07 12:50:39 -0600

    Calculadora Visual Simple
    Calculadora Visual Simple la que no tiene el teclado numerico

---
+++ calculadoraSimple/Presentacion/Calculadora.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace calculadora3.Presentacion
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dato1 = new System.Windows.Forms.TextBox();
+            this.dato2 = new System.Windows.Forms.TextBox();
+            this.operacion = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dato1
+            // 
+            this.dato1.Location = new System.Drawing.Point(177, 19);
+            this.dato1.Name = "dato1";
+            this.dato1.Size = new System.Drawing.Size(100, 22);
+            this.dato1.TabIndex = 4;
+            // 
+            // dato2
+            // 
+            this.dato2.Location = new System.Drawing.Point(177, 86);
+            this.dato2.Name = "dato2";
+            this.dato2.Size = new System.Drawing.Size(100, 22);
+            this.dato2.TabIndex = 5;
+            // 
+            // operacion
+            // 
+            this.operacion.Location = new System.Drawing.Point(177, 148);
+            this.operacion.Name = "operacion";
+            this.operacion.Size = new System.Drawing.Size(48, 22);
+            this.operacion.TabIndex = 6;
+            this.operacion.Text = "+";
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(177, 216);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(100, 22);
+            this.resultado.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(69, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 56);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Ver resultado";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Dato 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(34, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Dato 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(26, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Operacion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(29, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Resultado";
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.operacion);
+            this.Controls.Add(this.dato2);
+            this.Controls.Add(this.dato1);
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox dato1;
+        private System.Windows.Forms.TextBox dato2;
+        private System.Windows.Forms.TextBox operacion;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ calculadoraSimple/Presentacion/Calculadora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculadora3.Presentacion
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            //se inicia
+            InitializeComponent();
+        }
+
+        //boton 1 que es para ver el resultado
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //una variable
+            double res = 0;
+
+            //ver que esten dos caracteres en los campos correspondientes que en este caso son textBox
+            if (double.TryParse(dato1.Text, out double d1) && double.TryParse(dato2.Text, out double d2))
+            {
+                char op = operacion.Text[0];
+                switch (op)
+                {
+                    case '+':
+                        // Sumar los valores
+                        res = d1 + d2;
+                        break;
+                    case '-':
+                        // Sumar los valores
+                        res = d1 - d2;
+                        break;
+                    case '*':
+                        // Sumar los valores
+                        res = d1 * d2;
+                        break;
+                    case '/':
+                        // Sumar los valores
+                        res = d1 / d2;
+                        break;
+                    default:
+                        MessageBox.Show("Ingrese una operación valida");
+                        break;
+
+                }
+                // Mostrar el resultado en el TextBox correspondiente
+                resultado.Text = res.ToString();
+
+            }
+            else
+            {
+                //una alerta para ingresar solamente numeros
+                MessageBox.Show("Ingrese números válidos en los TextBox.");
+            }
+        }
+    }
+}
